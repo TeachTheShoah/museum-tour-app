f4fe50fe9ae7f3c2558f3e1509a3ad53254cd5a5
Nick <nzhong@andrew.cmu.edu>
2024-12-05 16:39:20 -0500

    modify footers

--- src/routes/tour/[slug]/+page.svelte
+++ src/routes/tour/[slug]/+page.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-  import { page } from '$app/stores';
-  import { get } from 'svelte/store';
-  import Map from "$lib/components/Map.svelte";
-  import Footer from "$lib/components/Footer.svelte";
+	import { page } from '$app/stores';
+	import { get } from 'svelte/store';
+	import Map from '$lib/components/Map.svelte';
+	import Footer from '$lib/components/Footer.svelte';
 
-  const p = get(page);
-  const id = p.params.slug;
+	const p = get(page);
+	const id = p.params.slug;
 </script>
 
-<Map { id }/>
+<Map {id} />
 <Footer />
